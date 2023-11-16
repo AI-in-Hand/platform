1b9d7cf1541bb3374bb42049ef88b6038fbcd8db
Nikita Bobrovskiy <n.bobrovskiy-ext@logispin.com>
2023-11-17 00:21:29 +0200

    Refactor

--- backend/functions.py
+++ backend/functions.py
@@ -1,46 +1,70 @@
-import os
 import json
+import os
 
-# Get the current directory of the script
-current_dir = os.path.dirname(os.path.realpath(__file__))
+ANALYST_DOC = "Software_Development_Guidelines_for_Analysts.txt"
 
-# Define the root directory where files are located
-ROOT_DIR = os.path.join(current_dir, 'data/')
 
-# Update the paths for the document files
-ANALYST_DOC = os.path.join(ROOT_DIR, "knowledge.pdf")
-DEVELOPER_DOC = os.path.join(ROOT_DIR, "Software_Development_Guidelines_for_Developers.docx")
+def get_root_dir():
+    """ Get the root directory where data files are located. """
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    return os.path.join(current_dir, 'data/')
+
+
+def get_assistant_file_path(root_dir):
+    """ Get the file path for assistant.json. """
+    return os.path.join(root_dir, 'assistant.json')
+
+
+def load_existing_assistant(file_path):
+    """ Load existing assistant data from file. """
+    try:
+        with open(file_path, 'r') as file:
+            return json.load(file)['assistant_id']
+    except (IOError, KeyError):
+        return None
+
+
+def create_new_assistant(client, root_dir):
+    """ Create a new assistant and save its ID. """
+    knowledge_doc = os.path.join(root_dir, ANALYST_DOC)
+
+    try:
+        with open(knowledge_doc, "rb") as doc_file:
+            file = client.files.create(file=doc_file, purpose='assistants')
+
+        assistant = client.beta.assistants.create(
+            instructions="""\
+The assistant, Software Development Analyst, has been programmed to gather user requirements. 
+A Document has been provided with information on the methodology \
+of this process. 
+Strictly follow the instructions to achieve a goal: to generate and output "requirements.txt" document 
+(in case the user project satisfies the limitations described in the attached Document). """,
+            model="gpt-4-1106-preview",
+            tools=[{"type": "retrieval"}],
+            file_ids=[file.id]
+        )
+
+        assistant_file_path = get_assistant_file_path(root_dir)
+        with open(assistant_file_path, 'w') as file:
+            json.dump({'assistant_id': assistant.id}, file)
+
+        return assistant.id
+    except IOError as e:
+        print(f"Error creating new assistant: {e}")
+        return None
 
 
 def create_assistant(client):
-    assistant_file_path = os.path.join(ROOT_DIR, 'assistant.json')
+    """ Create an assistant or load an existing one. """
+    root_dir = get_root_dir()
+    assistant_file_path = get_assistant_file_path(root_dir)
 
-    # Check if the assistant file exists and has content
+    assistant_id = None
     if os.path.exists(assistant_file_path) and os.path.getsize(assistant_file_path) > 0:
-        with open(assistant_file_path, 'r') as file:
-            assistant_data = json.load(file)
-            assistant_id = assistant_data['assistant_id']
-            print("Loaded existing assistant ID")
-    else:
-        file = client.files.create(file=open(ANALYST_DOC, "rb"),
-                                   purpose='assistants')
+        assistant_id = load_existing_assistant(assistant_file_path)
 
-        assistant = client.beta.assistants.create(instructions="""
-The assistant, Software Development Analyst, has been programmed to gather user requirements. 
-A Document "knowledge.pdf" has been provided with information on the methodology \
-of this process. 
-Strictly follow the instructions to achieve a goal: to generate and output "requirements.txt" document 
-(in case the user project satisfies the limitations described in the \
-"knowledge.pdf" Document). """,
-                                                  model="gpt-4-1106-preview",
-                                                  tools=[{
-                                                      "type": "retrieval"
-                                                  }],
-                                                  file_ids=[file.id])
-        with open(assistant_file_path, 'w') as file:
-            json.dump({'assistant_id': assistant.id}, file)
-        print("Created a new assistant and saved the ID.")
-
-        assistant_id = assistant.id
+    if assistant_id is None:
+        print("Creating a new assistant...")
+        assistant_id = create_new_assistant(client, root_dir)
 
     return assistant_id
