dac822a86cad795a43a58ce05f6266d9f5dbfa83
Nikita Bobrovskiy <n.bobrovskiy-ext@logispin.com>
2023-11-16 23:46:14 +0200

    Fix db

--- backend/functions.py
+++ backend/functions.py
@@ -1,14 +1,19 @@
 import os
 import json
 
-ROOT_DIR = ...
+# Get the current directory of the script
+current_dir = os.path.dirname(os.path.realpath(__file__))
 
-ANALYST_DOC = "Software_Development_Guidelines_for_Analysts.docx"
-DEVELOPER_DOC = "Software_Development_Guidelines_for_Developers.docx"
+# Define the root directory where files are located
+ROOT_DIR = os.path.join(current_dir, 'data/')
+
+# Update the paths for the document files
+ANALYST_DOC = os.path.join(ROOT_DIR, "knowledge.pdf")
+DEVELOPER_DOC = os.path.join(ROOT_DIR, "Software_Development_Guidelines_for_Developers.docx")
 
 
 def create_assistant(client):
-    assistant_file_path = 'assistant.json'
+    assistant_file_path = os.path.join(ROOT_DIR, 'assistant.json')
 
     # Check if the assistant file exists and has content
     if os.path.exists(assistant_file_path) and os.path.getsize(assistant_file_path) > 0:
@@ -22,10 +27,11 @@ def create_assistant(client):
 
         assistant = client.beta.assistants.create(instructions="""
 The assistant, Software Development Analyst, has been programmed to gather user requirements. 
-A Document has been provided with information on the methodology of this process. 
-Strictly follow the instructions to achieve a goal: to generate and output a Requirements Document 
-(in case the user project satisfies the limitations described in the Document).
-""",
+A Document "knowledge.pdf" has been provided with information on the methodology \
+of this process. 
+Strictly follow the instructions to achieve a goal: to generate and output "requirements.txt" document 
+(in case the user project satisfies the limitations described in the \
+"knowledge.pdf" Document). """,
                                                   model="gpt-4-1106-preview",
                                                   tools=[{
                                                       "type": "retrieval"
