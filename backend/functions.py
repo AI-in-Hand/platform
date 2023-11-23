9a14e7fb193664568e61500e19ee4adc5dd2d1a0
Nikita Bobrovskiy <39348559+bonk1t@users.noreply.github.com>
2023-11-23 22:30:19 +0200

    Refactor to focus on Software Requirement Gathering

--- backend/functions.py
+++ backend/functions.py
@@ -1,211 +1,54 @@
 import json
 import os
 
-import requests
 from openai import OpenAI
-from pyairtable import Api
 
-from prompts import assistant_instructions, formatter_prompt
+from prompts import assistant_instructions
 
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-GOOGLE_CLOUD_API_KEY = os.environ["GOOGLE_CLOUD_API_KEY"]
-AIRTABLE_API_KEY = os.environ["AIRTABLE_API_KEY"]
-
-# Init OpenAI Client
-client = OpenAI(api_key=OPENAI_API_KEY)
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
-# Add lead to Airtable
-def create_lead(name, phone):
-    api = Api(api_key=AIRTABLE_API_KEY)
-    table = api.table("Sales CRM", "Contacts")
-    data = {"Name": name, "Phone": phone}
-    resp = table.create(data=data)
-    return resp.json()
+TXT_FILE_NAME = "Requirement_Gathering_Guidelines_for_Analysts.txt"
 
 
-# Get coordinates from address via Geocoding API
-def get_coordinates(address):
-    geocoding_url = f"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={GOOGLE_CLOUD_API_KEY}"
-    response = requests.get(geocoding_url)
-    if response.status_code == 200:
-        location = response.json().get("results")[0].get("geometry").get("location")
-        print(f"Coordinates for {address}: {location}")
-        return location["lat"], location["lng"]
-    else:
-        print(f"Error getting coordinates: {response.text}")
-
-
-# Get solar data for coordinate from Solar API
-def get_solar_data(lat, lng):
-    solar_api_url = f"https://solar.googleapis.com/v1/buildingInsights:findClosest?location.latitude={lat}&location.longitude={lng}&requiredQuality=HIGH&key={GOOGLE_CLOUD_API_KEY}"
-    response = requests.get(solar_api_url)
-    if response.status_code == 200:
-        print("Solar data retrieved successfully.")
-        return response.json()
-    else:
-        print(f"Error getting solar data: {response.text}")
-
-
-# Extract financial data LIST from solar data
-def extract_financial_analyses(solar_data):
-    try:
-        return solar_data.get("solarPotential", {}).get("financialAnalyses", [])
-    except KeyError as e:
-        print(f"Data extraction error: {e}")
-
-
-# Get financial data for the address
-def get_financial_data_for_address(address):
-    lat, lng = get_coordinates(address)
-    if not lat or not lng:
-        return {"error": "Could not get coordinates for the address provided."}
-    return extract_financial_analyses(get_solar_data(lat, lng))
-
-
-# Match user's bill to scenario in the financial data
-def find_closest_financial_analysis(user_bill, financial_analyses):
-    closest_match = None
-    smallest_difference = float("inf")
-    for analysis in financial_analyses:
-        bill_amount = int(analysis.get("monthlyBill", {}).get("units", 0))
-        difference = abs(bill_amount - user_bill)
-        if difference < smallest_difference:
-            smallest_difference = difference
-            closest_match = analysis
-    return closest_match
-
-
-# Use GPT completion to extract most relevant data from financial analysis
-def simplify_financial_data(data):
-    try:
-        data_str = json.dumps(data, indent=2)
-
-        # Getting formatter prompt from "prompts.py" file
-        system_prompt = formatter_prompt
-
-        # Replace 'client' with your actual OpenAI client initialization.
-        completion = client.chat.completions.create(
-            model="gpt-4-1106-preview",
-            messages=[
-                {
-                    "role": "system",
-                    "content": system_prompt,  # Getting prompt from "prompts.py" file
-                },
-                {
-                    "role": "user",
-                    "content": f"Here is some data, parse and format it exactly as shown in the example: {data_str}",
-                },
-            ],
-            temperature=0,
-        )
-
-        simplified_data = json.loads(completion.choices[0].message.content)
-        print("Simplified Data:", simplified_data)
-        return simplified_data
-
-    except Exception as e:
-        print("Error simplifying data:", e)
-        return None
-
-
-# Main calculation function for solar data output
-def solar_panel_calculations(address, monthly_bill):
-    print(
-        f"Calculating solar panel potential for {address} with bill amount {monthly_bill}."
-    )
-    financial_analyses = get_financial_data_for_address(address)
-    if "error" in financial_analyses:
-        print(financial_analyses["error"])
-        return financial_analyses
-    closest_financial_analysis = find_closest_financial_analysis(
-        int(monthly_bill), financial_analyses
-    )
-    if closest_financial_analysis:
-        return simplify_financial_data(closest_financial_analysis)
-    else:
-        print("No suitable financial analysis found.")
-        return {"error": "No suitable financial analysis found for the given bill."}
-
-
-# Create or load assistant
 def create_assistant(client):
     assistant_file_path = "assistant.json"
 
-    # If there is an assistant.json file already, then load that assistant
     if os.path.exists(assistant_file_path):
         with open(assistant_file_path, "r") as file:
             assistant_data = json.load(file)
-            assistant_id = assistant_data["assistant_id"]
-            print("Loaded existing assistant ID.")
+            return assistant_data["assistant_id"]
     else:
-        # If no assistant.json is present, create a new assistant using the below specifications
-
-        # To change the knowledge document, modifiy the file name below to match your document
-        # If you want to add multiple files, paste this function into ChatGPT and ask for it to add support for multiple files
-        file = client.files.create(
-            file=open("knowledge.docx", "rb"), purpose="assistants"
-        )
-
+        with open(TXT_FILE_NAME, "rb") as file_to_read:
+            file = client.files.create(file=file_to_read, purpose="assistants")
         assistant = client.beta.assistants.create(
-            # Getting assistant prompt from "prompts.py" file, edit on left panel if you want to change the prompt
             instructions=assistant_instructions,
             model="gpt-4-1106-preview",
             tools=[
-                {
-                    "type": "retrieval"  # This adds the knowledge base as a tool
-                },
-                {
-                    "type": "function",  # This adds the solar calculator as a tool
-                    "function": {
-                        "name": "solar_panel_calculations",
-                        "description": "Calculate solar potential based on a given address and monthly electricity bill in USD.",
-                        "parameters": {
-                            "type": "object",
-                            "properties": {
-                                "address": {
-                                    "type": "string",
-                                    "description": "Address for calculating solar potential.",
-                                },
-                                "monthly_bill": {
-                                    "type": "integer",
-                                    "description": "Monthly electricity bill in USD for savings estimation.",
-                                },
-                            },
-                            "required": ["address", "monthly_bill"],
-                        },
-                    },
-                },
-                {
-                    "type": "function",  # This adds the lead capture as a tool
-                    "function": {
-                        "name": "create_lead",
-                        "description": "Capture lead details and save to Airtable.",
-                        "parameters": {
-                            "type": "object",
-                            "properties": {
-                                "name": {
-                                    "type": "string",
-                                    "description": "Name of the lead.",
-                                },
-                                "phone": {
-                                    "type": "string",
-                                    "description": "Phone number of the lead.",
-                                },
-                            },
-                            "required": ["name", "phone"],
-                        },
-                    },
-                },
+                {"type": "retrieval"},
+                {"type": "function"},
             ],
             file_ids=[file.id],
         )
-
-        # Create a new assistant.json file to load on future runs
         with open(assistant_file_path, "w") as file:
             json.dump({"assistant_id": assistant.id}, file)
-            print("Created a new assistant and saved the ID.")
+        return assistant.id
 
-        assistant_id = assistant.id
 
-    return assistant_id
+def handle_action(tool_call, thread_id, run_id):
+    if tool_call.function.name == "save_requirements":
+        arguments = json.loads(tool_call.function.arguments)
+        return save_requirements(arguments["user_responses"], thread_id, run_id)
+    # Additional function calls can be handled here
+
+
+def save_requirements(user_responses, thread_id, run_id):
+    file_path = f"requirements/{thread_id}_{run_id}.txt"
+    with open(file_path, "w") as file:
+        for response in user_responses:
+            file.write(f"{response['question']}: {response['answer']}\n")
+    return {
+        "status": "Success",
+        "message": "Requirements document saved.",
+        "path": file_path,
+    }
--- backend/prompts.py
+++ backend/prompts.py
@@ -1,54 +1,7 @@
-formatter_prompt = """
-You are a helpful data parsing assistant. You are given JSON with financial data
-and you filter it down to only a set of keys we want. This is the exact structure we need:
-
-{
-  "monthlyBill": "200",
-  "federalIncentive": "6815",
-  "stateIncentive": "4092",
-  "utilityIncentive": "3802",
-  "totalCostWithoutSolar": "59520",
-  "solarCoveragePercentage": 99.33029,
-  "leasingOption": {
-    "annualCost": "1539",
-    "firstYearSavings": "745",
-    "twentyYearSavings": "23155",
-    "presentValueTwentyYear": "14991"
-  },
-  "cashPurchaseOption": {
-    "outOfPocketCost": "30016",
-    "paybackYears": 7.75,
-    "firstYearSavings": "2285",
-    "twentyYearSavings": "53955",
-    "presentValueTwentyYear": "17358"
-  },
-  "financedPurchaseOption": {
-    "annualLoanPayment": "1539",
-    "firstYearSavings": "745",
-    "twentyYearSavings": "23155",
-    "presentValueTwentyYear": "14991"
-  }
-}
-
-If you cannot find a value for the key, then use "None Found". Please double check before using this fallback.
-Process ALL the input data provided by the user and output our desired JSON format exactly,
-ready to be converted into valid JSON with Python.
-Ensure every value for every key is included, particularly for each of the incentives.
-"""
-
 assistant_instructions = """
-    The assistant has been programmed to help customers of Smith's Solar to learn more about solar for their single-family home and to calculate estimated savings for them if they were to install solar on their home. The assistant is placed on the Smiths Solar website for customers to learn more about solar and the company's offerings.
-
-    A document has been provided with information on solar for single-family homes which can be used to answer the customer's questions. When using this information in responses, the assistant keeps answers short and relevant to the user's query.
-    Additionally, the assistant can perform solar savings calculations based on a given address and their monthly electricity bill in USD. When outputting their solar savings and key info, markdown formatting should be used for bolding key figures.
-    After the assistant has provided the user with their solar caluclations, they should ask for their name and phone number so that one of the team can get in contact with them about installing solar for their home.
-
-    With this information, the assistant can add the lead to the company CRM via the create_lead function, also pulling in the user's address that was mentioned prior. This should provide the name, email, and address of the customer to the create_lead function.
-"""
-
-assistant_instructions_main = """
-The assistant, Software Development Analyst, has been programmed to gather user requirements.
-A Document has been provided with information on the methodology of this process.
-Strictly follow the instructions to achieve a goal: to generate and output a Requirements Document
-(in case the user project satisfies the limitations described in the Document).
+The assistant, Software Development Analyst, \
+has been programmed to gather user requirements for software development projects.
+It uses a document provided with information on the methodology of this process.
+The assistant guides the user through stages of requirement gathering, \
+asking relevant questions and recording responses.
 """
