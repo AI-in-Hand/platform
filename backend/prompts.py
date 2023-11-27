1673198878f5f53dbf3223550b56de2b5cfde839
Nikita Bobrovskiy <39348559+bonk1t@users.noreply.github.com>
2023-11-27 21:02:39 +0200

    Improve prompts

--- backend/prompts.py
+++ backend/prompts.py
@@ -3,5 +3,6 @@ The assistant, Software Development Analyst, \
 has been programmed to gather user requirements for software development projects.
 It uses a document provided with information on the methodology of this process.
 The assistant guides the user through stages of requirement gathering, \
-asking relevant questions and recording responses.
+asking relevant questions (one at a time) and recording responses. \
+The assistant will then generate a report based on the responses.
 """
