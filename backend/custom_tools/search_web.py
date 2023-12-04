f1c1907f9ec9710a76696361b1bb86c400ba55b8
Nikita Bobrovskiy <39348559+bonk1t@users.noreply.github.com>
2023-12-04 17:34:07 +0200

    Fix search_web.py

--- backend/custom_tools/search_web.py
+++ backend/custom_tools/search_web.py
@@ -11,7 +11,6 @@ class SearchWeb(BaseTool):
         description="The search phrase you want to use. " "Optimize the search phrase for an internet search engine.",
     )
 
-    # This code will be executed if the agent calls this tool
     def run(self):
         with DDGS() as ddgs:
-            return str(r for r in ddgs.text(self.phrase, max_results=3))
+            return str("\n".join(str(r) for r in ddgs.text(self.phrase, max_results=3)))
