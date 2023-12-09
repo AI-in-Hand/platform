f640c5dba913d94c2bd02edd6305a7653a5a18ec
Nikita Bobrovskiy <39348559+bonk1t@users.noreply.github.com>
2023-12-09 18:23:55 +0000

    Cleanup

--- backend/constants.py
+++ backend/constants.py
@@ -4,33 +4,3 @@ from pathlib import Path
 DATA_DIR = Path(__file__).resolve().parent / "data"
 DEFAULT_CONFIG_FILE = DATA_DIR / "default_config.json"
 CONFIG_FILE = DATA_DIR / "config"
-
-# Assistant Tools Configuration
-ASSISTANT_TOOLS = [
-    {"type": "retrieval"},
-    {
-        "type": "function",
-        "function": {
-            "name": "save_requirements",
-            "description": "Save requirements document.",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "user_responses": {
-                        "type": "array",
-                        "items": {
-                            "type": "object",
-                            "properties": {
-                                "question": {"type": "string"},
-                                "answer": {"type": "string"},
-                            },
-                            "required": ["question", "answer"],
-                        },
-                    },
-                    "requirements_text": {"type": "string"},
-                },
-            },
-            "required": ["user_responses", "requirements_text"],
-        },
-    },
-]
