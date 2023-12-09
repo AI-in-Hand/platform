a8c20984bf07218a526a51e737a1634033b0c5d0
Nikita Bobrovskiy <39348559+bonk1t@users.noreply.github.com>
2023-12-09 05:15:57 +0000

    Add Code Interpreter

--- backend/custom_tools/__init__.py
+++ backend/custom_tools/__init__.py
@@ -1,4 +1,6 @@
 # Mapping tool names to actual tool classes
+from agency_swarm.tools import CodeInterpreter, Retrieval
+
 from custom_tools.build_directory_tree import BuildDirectoryTree
 from custom_tools.generate_proposal import GenerateProposal
 from custom_tools.print_all_files_in_directory import PrintAllFilesInDirectory
@@ -6,6 +8,8 @@ from custom_tools.search_web import SearchWeb
 from custom_tools.write_and_save_program import WriteAndSaveProgram
 
 TOOL_MAPPING = {
+    "CodeInterpreter": CodeInterpreter,
+    "Retrieval": Retrieval,
     "BuildDirectoryTree": BuildDirectoryTree,
     "GenerateProposal": GenerateProposal,
     "PrintAllFilesInDirectory": PrintAllFilesInDirectory,
