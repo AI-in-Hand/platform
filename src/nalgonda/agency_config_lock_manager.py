2b3080d97715cc0b54a72a5c5b87bdac0f32dfeb
Nikita Bobrovskiy <39348559+bonk1t@users.noreply.github.com>
2023-12-10 14:47:29 +0000

    Update agency_config_lock_manager.py

--- src/nalgonda/agency_config_lock_manager.py
+++ src/nalgonda/agency_config_lock_manager.py
@@ -1,13 +1,11 @@
 import threading
-
+from collections import defaultdict
 
 class AgencyConfigLockManager:
     """Lock manager for agency config files"""
 
-    _locks: dict[str, threading.Lock] = {}
+    _locks: dict[str, threading.Lock] = defaultdict(threading.Lock)
 
     @classmethod
     def get_lock(cls, agency_id):
-        if agency_id not in cls._locks:
-            cls._locks[agency_id] = threading.Lock()
         return cls._locks[agency_id]
