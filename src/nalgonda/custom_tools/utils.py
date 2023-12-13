8b56ab18ff1e328d24b173f6d520a20d39e962c1
Guilherme Parpinelli <guilheerme.p@gmail.com>
2023-12-13 00:07:36 +0000

    chore: extract logic for reuse

--- src/nalgonda/custom_tools/utils.py
+++ src/nalgonda/custom_tools/utils.py
@@ -1,8 +1,12 @@
+import tempfile
+from pathlib import Path
+
 from agency_swarm.util import get_openai_client
 
 
 def get_chat_completion(user_prompt, system_message, **kwargs) -> str:
-    """Generate a chat completion based on a prompt and a system message.
+    """
+    Generate a chat completion based on a prompt and a system message.
     This function is a wrapper around the OpenAI API.
     """
     from config import settings
@@ -24,3 +28,17 @@ def get_chat_completion(user_prompt, system_message, **kwargs) -> str:
     )
 
     return str(completion.choices[0].message.content)
+
+
+def check_directory_traversal(dir_path: str) -> Path:
+    """
+    Ensures that the given directory path is within allowed paths.
+    """
+    path = Path(dir_path)
+    if ".." in path.parts:
+        raise ValueError("Directory traversal is not allowed.")
+
+    allowed_bases = [Path(tempfile.gettempdir()).resolve(), Path.home().resolve()]
+    if not any(str(path.resolve()).startswith(str(base)) for base in allowed_bases):
+        raise ValueError("Directory traversal is not allowed.")
+    return path
