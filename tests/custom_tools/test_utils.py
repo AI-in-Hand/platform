0aa926ae2daf1fd146e04c5f49fbbc2163a24855
Guilherme Parpinelli <guilheerme.p@gmail.com>
2023-12-13 00:08:48 +0000

    add tests

---
+++ tests/custom_tools/test_utils.py
@@ -0,0 +1,10 @@
+import pytest
+
+from nalgonda.custom_tools.utils import check_directory_traversal
+
+@pytest.mark.parametrize("path", ["..", "/", "/sbin"])
+def test_check_directory_traversal_raises_for_attempts(path):
+    with pytest.raises(ValueError) as e:
+        check_directory_traversal(path)
+    assert e.errisinstance(ValueError)
+    assert "Directory traversal is not allowed." in str(e.value)
