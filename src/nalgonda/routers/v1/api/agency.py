2996c5a7a49051ba677d4fc0c0c2cf350da86b67
Nikita Bobrovskiy <39348559+bonk1t@users.noreply.github.com>
2023-12-14 04:15:20 +0200

    API endpoints

--- src/nalgonda/routers/v1/api/agency.py
+++ src/nalgonda/routers/v1/api/agency.py
@@ -1,11 +1,10 @@
-import asyncio
 import logging
 import uuid
 
 from agency_manager import AgencyManager
 from agency_swarm import Agency
-from agency_swarm.messages import MessageOutput
 from fastapi import APIRouter
+from models.request_models import AgencyMessagePostRequest
 
 logger = logging.getLogger(__name__)
 agency_manager = AgencyManager()
@@ -26,27 +25,22 @@ async def create_agency():
 
 
 @agency_api_router.post("/message")
-async def send_message(agency_id: str, message: str):
+async def send_message(payload: AgencyMessagePostRequest) -> dict:
     """Send a message to the CEO of the given agency."""
     # TODO: Add authentication: check if agency_id is valid for the given user
 
-    logger.info(f"Received message: {message} for agency: {agency_id}")
-    agency = await agency_manager.get_or_create_agency(agency_id=agency_id)
+    logger.info(f"Received message: {payload.message} for agency: {payload.agency_id}")
+    agency = await agency_manager.get_or_create_agency(agency_id=payload.agency_id)
 
     try:
-        await process_message(message, agency)
+        response = await process_message(payload.message, agency)
+        return {"response": response}
     except Exception as e:
         logger.exception(e)
+        return {"error": str(e)}
 
 
 async def process_message(user_message: str, agency: Agency) -> str:
     """Process a message from the user and return the response from the CEO."""
-    loop = asyncio.get_running_loop()
-
-    gen = agency.get_completion(message=user_message, yield_messages=False)  # False: get only the final CEO message
-
-    def get_next() -> MessageOutput:
-        return next(gen)
-
-    response = await loop.run_in_executor(None, get_next)
-    return response.get_formatted_content()
+    response = agency.get_completion(message=user_message, yield_messages=False)
+    return response
