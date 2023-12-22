0aa927f63cfa4add5949358967d8f2e4da00f617
Nikita Bobrovskiy <39348559+bonk1t@users.noreply.github.com>
2023-12-22 18:48:47 +0000

    Comment fixes

--- nalgonda/settings.py
+++ nalgonda/settings.py
@@ -1,16 +1,19 @@
-from pydantic import Field
+from pydantic import AliasChoices, Field, RedisDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 LATEST_GPT_MODEL = "gpt-4-1106-preview"
 
 
 class Settings(BaseSettings):
-    openai_api_key: str | None = Field(default=None, validation_alias="OPENAI_API_KEY")
-    gpt_model: str = Field(default=LATEST_GPT_MODEL, validation_alias="GPT_MODEL")
-    google_credentials: str | None = Field(default=None, validation_alias="GOOGLE_CREDENTIALS")
-    redis_url: str = Field(default="redis://localhost:6379", env="REDIS_URL")
+    openai_api_key: str | None = Field(default=None)
+    gpt_model: str = Field(default=LATEST_GPT_MODEL)
+    google_credentials: str | None = Field(default=None)
+    redis_dsn: RedisDsn = Field(
+        "redis://localhost:6379/1",
+        validation_alias=AliasChoices("service_redis_dsn", "redis_url"),
+    )
 
-    model_config = SettingsConfigDict(env_file=".env", case_sensitive=True)
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
