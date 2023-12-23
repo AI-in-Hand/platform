9a5a95a5303519a9e1182a7cbef67af2d67ce0a3
Nikita Bobrovskiy <39348559+bonk1t@users.noreply.github.com>
2023-12-23 17:32:43 +0000

    Fix Pickling

--- nalgonda/caching/redis_cache_manager.py
+++ nalgonda/caching/redis_cache_manager.py
@@ -2,6 +2,7 @@ import asyncio
 import pickle
 
 from agency_swarm import Agency
+from agency_swarm.util import get_openai_client
 from redis import asyncio as aioredis
 
 from nalgonda.caching.cache_manager import CacheManager
@@ -24,11 +25,17 @@ class RedisCacheManager(CacheManager):
     async def get(self, key: str) -> Agency | None:
         """Gets the value for the given key from the cache"""
         serialized_data = await self.redis.get(key)
-        return pickle.loads(serialized_data) if serialized_data else None
+        if not serialized_data:
+            return None
+
+        loaded = pickle.loads(serialized_data)
+        loaded = self.restore_client_objects(loaded)
+        return loaded
 
     async def set(self, key: str, value: Agency) -> None:
         """Sets the value for the given key in the cache"""
-        serialized_data = pickle.dumps(value)
+        value_copy = self.remove_client_objects(value)
+        serialized_data = pickle.dumps(value_copy)
         await self.redis.set(key, serialized_data)
 
     async def delete(self, key: str) -> None:
@@ -38,3 +45,23 @@ class RedisCacheManager(CacheManager):
     async def close(self) -> None:
         """Closes the Redis connection"""
         await self.redis.close()
+
+    @staticmethod
+    def remove_client_objects(agency: Agency) -> Agency:
+        """Remove all client objects from the agency object"""
+        for agent in agency.agents:
+            agent.client = None
+
+        agency.main_thread.client = None
+
+        return agency
+
+    @staticmethod
+    def restore_client_objects(agency: Agency) -> Agency:
+        """Restore all client objects from the agency object"""
+        for agent in agency.agents:
+            agent.client = get_openai_client()
+
+        agency.main_thread.client = get_openai_client()
+
+        return agency
