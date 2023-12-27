0bb3539831b2414da959f688910c82d5457fa9cf
Nikita Bobrovskiy <39348559+bonk1t@users.noreply.github.com>
2023-12-27 13:33:42 +0200

    Add SaveLeadToAirtable tool

--- nalgonda/settings.py
+++ nalgonda/settings.py
@@ -5,14 +5,18 @@ LATEST_GPT_MODEL = "gpt-4-1106-preview"
 
 
 class Settings(BaseSettings):
+    algorithm: str = Field(default="HS256")
+    access_token_expire_minutes: int = Field(default=30)
+    airtable_token: str | None = Field(default=None)
+    airtable_base_id: str | None = Field(default=None)
+    airtable_table_id: str | None = Field(default=None)
+
     google_credentials: str | None = Field(default=None)
     gpt_model: str = Field(default=LATEST_GPT_MODEL)
     openai_api_key: str | None = Field(default=None)
     redis_tls_url: RedisDsn | None = Field(default=None)
     redis_url: RedisDsn = Field(default="redis://localhost:6379/1")
     secret_key: str | None = Field(default=None)
-    algorithm: str = Field(default="HS256")
-    access_token_expire_minutes: int = Field(default=30)
 
     model_config = SettingsConfigDict(env_file=".env")
 
