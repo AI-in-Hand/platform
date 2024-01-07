99283ef0123edcad35d1a9c2f61e3af1e7e8b39b
Nikita Bobrovskiy <39348559+bonk1t@users.noreply.github.com>
2024-01-07 23:12:01 +0200

    Update tests

--- tests/routers/v1/api/test_agency_endpoints.py
+++ tests/routers/v1/api/test_agency_endpoints.py
@@ -1,7 +1,6 @@
-from unittest.mock import patch
+from unittest.mock import AsyncMock, patch
 
 from nalgonda.models.agency_config import AgencyConfig
-from nalgonda.persistence.agency_config_firestore_storage import AgencyConfigFirestoreStorage
 
 
 def test_get_agency_config(client, mock_firestore_client):
@@ -19,8 +18,7 @@ def test_get_agency_config(client, mock_firestore_client):
     assert response.json() == mock_data
 
 
-@patch.object(AgencyConfigFirestoreStorage, "save")
-def test_update_agency_config_success(mock_save, client, mock_firestore_client):
+def test_update_agency_config_success(client, mock_firestore_client):
     # Setup initial data in mock Firestore client
     initial_data = {
         "agency_id": "test_agency",
@@ -30,22 +28,17 @@ def test_update_agency_config_success(mock_save, client, mock_firestore_client):
         "agency_chart": [],
     }
     mock_firestore_client.setup_mock_data("agency_configs", "test_agency", initial_data)
-
     new_data = {"agency_manifesto": "Updated Manifesto"}
-    response = client.put("/v1/api/agency/config?agency_id=test_agency", json=new_data)
+
+    with patch(
+        "nalgonda.dependencies.agency_manager.AgencyManager.update_agency", new_callable=AsyncMock
+    ) as mock_update_agency:
+        response = client.put("/v1/api/agency/config?agency_id=test_agency", json=new_data)
+
+    mock_update_agency.assert_called_once_with(AgencyConfig.model_validate(initial_data), new_data)
 
     assert response.status_code == 200
     assert response.json() == {"message": "Agency configuration updated successfully"}
-
-    # Verify that the save method was called with the updated configuration
-    updated_config = AgencyConfig(
-        agency_id="test_agency",
-        owner_id="test_owner",
-        agency_manifesto="Updated Manifesto",
-        agents=[],
-        agency_chart=[],
-    )
-    mock_save.assert_called_once_with(updated_config)
 
 
 def test_get_agency_config_not_found(client, mock_firestore_client):  # noqa: ARG001
