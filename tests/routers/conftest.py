cae5bd70769b1b8ea31dbef02d195ffdd640bbeb
Nikita Bobrovskiy <39348559+bonk1t@users.noreply.github.com>
2024-01-10 00:16:02 +0200

    Fix the tests

--- tests/routers/conftest.py
+++ tests/routers/conftest.py
@@ -2,10 +2,10 @@ import pytest
 from fastapi.testclient import TestClient
 
 from nalgonda.dependencies.auth import get_current_active_user
-from nalgonda.main import app
+from nalgonda.main import app, v1_api_app
 from tests.test_utils import get_current_active_user_override
 
-app.dependency_overrides[get_current_active_user] = get_current_active_user_override
+v1_api_app.dependency_overrides[get_current_active_user] = get_current_active_user_override
 
 
 @pytest.fixture
