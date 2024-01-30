16b302fd96cf074650c405a611ff12ef21f72fcd
Nikita Bobrovskiy <39348559+bonk1t@users.noreply.github.com>
2024-01-30 21:54:16 +0000

    Add print_all_files_in_path.py

--- nalgonda/custom_tools/__init__.py
+++ nalgonda/custom_tools/__init__.py
@@ -3,6 +3,7 @@ from agency_swarm.tools import CodeInterpreter, Retrieval
 
 from nalgonda.custom_tools.build_directory_tree import BuildDirectoryTree
 from nalgonda.custom_tools.generate_proposal import GenerateProposal
+from nalgonda.custom_tools.print_all_files_in_path import PrintAllFilesInPath
 from nalgonda.custom_tools.print_file_contents import PrintFileContents
 from nalgonda.custom_tools.save_lead_to_airtable import SaveLeadToAirtable
 from nalgonda.custom_tools.search_web import SearchWeb
@@ -14,6 +15,7 @@ TOOL_MAPPING = {
     "Retrieval": Retrieval,
     "BuildDirectoryTree": BuildDirectoryTree,
     "GenerateProposal": GenerateProposal,
+    "PrintAllFilesInPath": PrintAllFilesInPath,
     "PrintFileContents": PrintFileContents,
     "SaveLeadToAirtable": SaveLeadToAirtable,
     "SearchWeb": SearchWeb,
