0c25667bf7b384a2f8fdbff6ec3cbf2f4c9ce828
Nikita Bobrovskiy <39348559+bonk1t@users.noreply.github.com>
2024-01-31 17:13:15 +0000

    add a type hint

--- nalgonda/exception_handlers.py
+++ nalgonda/exception_handlers.py
@@ -7,7 +7,7 @@ from fastapi.responses import JSONResponse
 logger = logging.getLogger(__name__)
 
 
-async def bad_request_exception_handler(request: Request, exc: ValueError):  # noqa: ARG001
+def bad_request_exception_handler(request: Request, exc: ValueError) -> JSONResponse:  # noqa: ARG001
     return JSONResponse(
         status_code=HTTPStatus.BAD_REQUEST,
         content={"detail": str(exc)},
