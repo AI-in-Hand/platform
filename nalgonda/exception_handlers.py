6424713826978c7fe06734dbbbcefca681c054c8
Nikita Bobrovskiy <39348559+bonk1t@users.noreply.github.com>
2024-03-25 17:05:42 +0000

    Improve logging

--- nalgonda/exception_handlers.py
+++ nalgonda/exception_handlers.py
@@ -7,16 +7,18 @@ from fastapi.responses import JSONResponse
 logger = logging.getLogger(__name__)
 
 
-def bad_request_exception_handler(request: Request, exc: ValueError) -> JSONResponse:  # noqa: ARG001
+def bad_request_exception_handler(request: Request, exc: ValueError) -> JSONResponse:
+    # Log the exception for debugging purposes
+    logger.exception(f"request: {request} exc: {exc}")
     return JSONResponse(
         status_code=HTTPStatus.BAD_REQUEST,
         content={"detail": str(exc)},
     )
 
 
-def unhandled_exception_handler(request: Request, exc: Exception) -> JSONResponse:  # noqa: ARG001
+def unhandled_exception_handler(request: Request, exc: Exception) -> JSONResponse:
     # Log the exception for debugging purposes
-    logger.exception(exc)
+    logger.exception(f"request: {request} exc: {exc}")
     return JSONResponse(
         status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
         content={"detail": "Internal server error"},
