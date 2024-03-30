dfba9a42ff2495f2a3b835d0d3acf59643824093
Nikita Bobrovskiy <39348559+bonk1t@users.noreply.github.com>
2024-03-30 04:07:42 +0000

    getAuth bug fix

--- frontend/src/firebase/firebase-config.js
+++ frontend/src/firebase/firebase-config.js
@@ -1,21 +1,17 @@
-import { initializeApp, getApps } from "firebase/app";
+import { FirebaseOptions, initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
-const firebaseConfig = {
+const config: FirebaseOptions = {
   apiKey: "AIzaSyCWIQCLyRF5FD5uQdE4M9l-EDN-NnP09Yg",
-   authDomain: "ai-in-hand.firebaseapp.com",
-   projectId: "ai-in-hand",
-   storageBucket: "ai-in-hand.appspot.com",
-   messagingSenderId: "86070783905",
-   appId: "1:86070783905:web:2b9eed73fcf06902b7a5e0",
-   measurementId: "G-D74MR0JLD5"
+  authDomain: "ai-in-hand.firebaseapp.com",
+  projectId: "ai-in-hand",
+  storageBucket: "ai-in-hand.appspot.com",
+  messagingSenderId: "86070783905",
+  appId: "1:86070783905:web:2b9eed73fcf06902b7a5e0",
+  measurementId: "G-D74MR0JLD5"
 };
 
-// Initialize Firebase only if there are no initialized apps
-let app;
-if (!getApps().length) {
-  app = initializeApp(firebaseConfig);
-}
+const app = !getApps().length ? initializeApp(config) : getApp();
+const auth = getAuth(app);
 
-export const auth = getAuth(app);
+export { auth };
