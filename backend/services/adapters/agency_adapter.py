38ef03378115fa81bcc53b45112735ff7987a7f7
Nikita Bobrovskiy <39348559+bonk1t@users.noreply.github.com>
2024-04-21 20:29:13 +0100

    BE

--- backend/services/adapters/agency_adapter.py
+++ backend/services/adapters/agency_adapter.py
@@ -1,4 +1,4 @@
-from backend.models.agency_config import AgencyConfig, AgencyConfigForAPI
+from backend.models.agency_config import AgencyConfig, AgencyConfigForAPI, CommunicationFlow
 from backend.repositories.agent_flow_spec_storage import AgentFlowSpecStorage
 from backend.services.adapters.agent_adapter import AgentAdapter
 
@@ -7,9 +7,7 @@ class AgencyAdapter:
     """
     Adapter for the AgencyConfig model. Transforms the data from the frontend format to the model and vice versa.
     In particular, it converts the `agents` field with a list of IDs (AgencyConfig Pydantic model)
-    and main_agent and agency_chart fields with names of the agents into AgentFlowSpec objects in 2 fields:
-    - main_agent (`sender` in the frontend)
-    - agency_chart (`sender` + `receiver` in the frontend);
+    and main_agent and agency_chart fields with names of the agents into AgentFlowSpec objects in the `flows` field;
     and vice versa.
     """
 
@@ -25,19 +23,19 @@ class AgencyAdapter:
         - `main_agent` and `agency_chart` with names of the agents.
         agency_chart has a form: {"0": [sender_name, receiver_name]}.
         """
-        sender = [agency_config.sender.id] if agency_config.sender else []
-        receiver = [agency_config.receiver.id] if agency_config.receiver else []
-        agents = sender + receiver
-        # TODO: support multiple rows in the agency_chart (not implemented in the frontend)
-        agency_chart = (
-            {"0": [agency_config.sender.config.name, agency_config.receiver.config.name]}
-            if agency_config.sender and agency_config.receiver
-            else {}
-        )
+        agents = []
+        agency_chart = {}
+        for i, flow in enumerate(agency_config.flows):
+            sender_id = flow.sender.id
+            receiver_id = flow.receiver.id if flow.receiver else None
+            agents.extend([sender_id, receiver_id] if receiver_id else [sender_id])
+            agency_chart[str(i)] = [flow.sender.config.name, flow.receiver.config.name if flow.receiver else None]
+
+        main_agent = agency_config.flows[0].sender.config.name if agency_config.flows else None
 
         agency_config_dict = agency_config.model_dump()
-        agency_config_dict["agents"] = agents
-        agency_config_dict["main_agent"] = agency_config.sender.config.name if agency_config.sender else None
+        agency_config_dict["agents"] = list(set(agents))  # Remove duplicates
+        agency_config_dict["main_agent"] = main_agent
         agency_config_dict["agency_chart"] = agency_chart
         return AgencyConfig(**agency_config_dict)
 
@@ -52,12 +50,20 @@ class AgencyAdapter:
         """
         if not agency_config.agents:
             return AgencyConfigForAPI(**agency_config.model_dump())
+
         agent_list = self.agent_flow_spec_storage.load_by_ids(agency_config.agents)
         agents = {agent.config.name: agent for agent in agent_list}
-        sender = agents[agency_config.main_agent] if agency_config.main_agent else None
-        receiver = agents[agency_config.agency_chart["0"][1]] if agency_config.agency_chart else None
+
+        flows = []
+        for sender_name, receiver_name in agency_config.agency_chart.values():
+            sender = agents[sender_name]
+            receiver = agents[receiver_name] if receiver_name else None
+            flow = CommunicationFlow(
+                sender=self.agent_adapter.to_api(sender),
+                receiver=self.agent_adapter.to_api(receiver) if receiver else None,
+            )
+            flows.append(flow)
 
         agency_config_dict = agency_config.model_dump()
-        agency_config_dict["sender"] = self.agent_adapter.to_api(sender) if sender else None
-        agency_config_dict["receiver"] = self.agent_adapter.to_api(receiver) if receiver else None
+        agency_config_dict["flows"] = flows
         return AgencyConfigForAPI(**agency_config_dict)
