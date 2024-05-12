7a31e08a3512ae80ab5419b9317f2f86f3d1e61f
Nikita Bobrovskiy <39348559+bonk1t@users.noreply.github.com>
2024-05-12 21:32:10 +0100

    Add S3 storage adapter

--- backend/repositories/s3_storage.py
+++ backend/repositories/s3_storage.py
@@ -2,12 +2,15 @@ import boto3
 
 
 class S3Handler:
+    """A class to handle S3 operations."""
+
     def __init__(self, aws_access_key_id: str, aws_secret_access_key: str, bucket_name: str):
         self.aws_access_key_id = aws_access_key_id
         self.aws_secret_access_key = aws_secret_access_key
         self.bucket_name = bucket_name
 
     def upload_file(self, file_name: str, body: str) -> str:
+        """Upload a file to the S3 bucket."""
         s3 = boto3.client(
             "s3", aws_access_key_id=self.aws_access_key_id, aws_secret_access_key=self.aws_secret_access_key
         )
@@ -24,6 +27,7 @@ class S3Handler:
         return [f"{obj['Key']} - {obj['Size']} bytes" for obj in response["Contents"]]
 
     def download_file(self, file_name: str) -> str:
+        """Download a file from the S3 bucket."""
         s3 = boto3.client(
             "s3", aws_access_key_id=self.aws_access_key_id, aws_secret_access_key=self.aws_secret_access_key
         )
@@ -31,6 +35,7 @@ class S3Handler:
         return response["Body"].read().decode("utf-8")
 
     def delete_file(self, file_name: str) -> str:
+        """Delete a file from the S3 bucket."""
         s3 = boto3.client(
             "s3", aws_access_key_id=self.aws_access_key_id, aws_secret_access_key=self.aws_secret_access_key
         )
