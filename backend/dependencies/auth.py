c34909efd82c5dc42ca767aed75c71ffc6b8714e
Nick Bobrowski <39348559+bonk1t@users.noreply.github.com>
2024-06-01 11:35:19 +0100

    Adjust logging levels  (#128)
    * Update auth_service.py
    
    * Update auth.py

--- backend/dependencies/auth.py
+++ backend/dependencies/auth.py
@@ -24,6 +24,6 @@ async def get_current_superuser(
     current_user: Annotated[User, Depends(get_current_user)],
 ) -> User:
     if not current_user.is_superuser:
-        logger.error(f"User {current_user.id} is not a superuser")
+        logger.warning(f"User {current_user.id} is not a superuser")
         raise HTTPException(status_code=HTTPStatus.FORBIDDEN, detail="The user doesn't have enough privileges")
     return current_user
