ec8db003f04abb7f3f005ebb208b084a42a4e574
jrtanmoy <roy28301@gmail.com>
2024-07-10 16:49:30 +0600

    Added profile page with fields for First Name, Last Name, Email, and Notification settings.

---
+++ frontend/src/components/views/builder/profile.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import { Form, Input, Checkbox, Button } from "antd";
+
+const ProfilePage = ({ data }: any) => {
+  const [form] = Form.useForm();
+
+  const handleFinish = (values: any) => {
+    // console.log("Form values:", values);
+  };
+
+  return (
+    <div className="flex justify-center items-center">
+      <div className="w-full ">
+          <h1 className="text-3xl font-bold mb-8">Profile</h1>
+          <Form
+            form={form}
+            onFinish={handleFinish}
+            layout="vertical"
+            className="space-y-6"
+          >
+            <div className="grid grid-cols-12 gap-4">
+              <Form.Item
+                label="First Name"
+                name="firstName"
+                rules={[{ message: "Please input your first name!" }]}
+                className="col-span-12 sm:col-span-6 mb-4"
+              >
+                <Input className="rounded-md" />
+              </Form.Item>
+              <Form.Item
+                label="Last Name"
+                name="lastName"
+                rules={[{ message: "Please input your last name!" }]}
+                className="col-span-12 sm:col-span-6 mb-4"
+              >
+                <Input className="rounded-md" />
+              </Form.Item>
+            </div>
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                { message: "Please input your email!" },
+                { type: 'email', message: "Please enter a valid email address!" }
+              ]}
+              className="mb-4"
+            >
+              <Input className="rounded-md" />
+            </Form.Item>
+            <Form.Item name="notifications" valuePropName="checked" className="mb-4">
+              <Checkbox>Subscribe to Mailchimp notifications</Checkbox>
+            </Form.Item>
+            <Form.Item className="flex">
+              <Button type="primary" htmlType="submit" className="rounded-md px-8">
+                Save
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
